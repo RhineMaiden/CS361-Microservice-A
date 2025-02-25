0e8608283f906a1f0c318fc04d8493c43c02dfb0
Lorelei Klohr <lorelei@klohr.com>
2025-02-24 16:11:09 -0800

    added more flashcard data

--- microserviceA.py
+++ microserviceA.py
@@ -1,13 +1,11 @@
 import zmq #for ZeroMQ
 import random #for random.choice
-import time #for sleep
 
 context = zmq.Context() #set up enviroment to be able to create sockets
 socket = context.socket(zmq.REP) #specify type as reply socket
 socket.bind("tcp://*:4666") #where the socket will listen
 
 examplesList = [
-    [b"Assembly", b"byte", b"8 bits"],
     [b"Intro to Security", b"Malware", b"Malicious software"],
     [b"Intro to Security", b"Nonce", b"Seed or randomization value"],
     [b"Theory of Computation", b"Bitstring", b"Binary string, string that consists of 0s and 1s"],
@@ -17,7 +15,12 @@ examplesList = [
     [b"Psychology", b"Delerium", b"A mental state characterized by confusion and disorganized thinking"],
     [b"Statistics", b"Hypothesis", b"A Testable, educated guess"],
     [b"Psychology", b"Discrimintation", b"Actions based off of steriotypes that negatively affect an out group"],
-    [b"Psychology", b"Most common mental disorder in the US", b"Major Depressive Disorder"]
+    [b"Calculus", b"Rate of Change", b"A measure of the speed at which a variable changes over a specific period of time"],
+    [b"World Religion", b"Theocracy", b"A system of government in which priests rule in the name of God or a god"],
+    [b"Geology", b"Aftershock", b"A small earthquake that follows a main shock"],
+    [b"Geology", b"Subduction", b"A tectonic plate being pushed down into the mantle by another plate"],
+    [b"Geology", b"Weathering", b"Breakdown of rocks and minerals by physical and chemical processes"],
+    [b"Geology", b"Delta", b"Deposition where river enters a lake or ocean"]
 ]
 
 #create loop to wait for message from client
