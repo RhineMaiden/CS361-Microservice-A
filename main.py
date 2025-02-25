cee1231e05e1dc0379f1bde543af994537b543c8
Lorelei Klohr <lorelei@klohr.com>
2025-02-24 16:15:22 -0800

    user input and loop

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import zmq #for ZeroMQ
-import time #for sleep
 
 context = zmq.Context() #set up environment to be able to create sockets
 socket = context.socket(zmq.REQ) #specify type as request socket
 socket.connect("tcp://localhost:4666") #connect to remote socket
 
 #message to send
-message = "example"
+message = input("Enter message for microservice: ")
 
-socket.send_string(message) #sendin the specified string
-print(f"\n\nMessage sent: {message}\n") #print the sent message
+while message != "quit":
 
-message = socket.recv_multipart() #get the reply
-print(f"Message received: {message}\n") #print the reply message
+    socket.send_string(message) #sendin the specified string
+    print(f"\n\nMessage sent: {message}\n") #print the sent message
 
-message = [msg.decode('utf-8') for msg in message]
-print(f"Message decoded: {message}\n")
+    message = socket.recv_multipart() #get the reply
+    print(f"Message received: {message}\n") #print the reply message
 
-print(f"Course (message[0]): {message[0]}")
-print(f"Term (message[1]): {message[1]}")
-print(f"Definition (message[2]): {message[2]}\n")
+    message = [msg.decode('utf-8') for msg in message]
+    print(f"Message decoded: {message}\n")
+
+    print(f"Course (message[0]): {message[0]}")
+    print(f"Term (message[1]): {message[1]}")
+    print(f"Definition (message[2]): {message[2]}\n")
+
+    message = input("Enter message for microservice: ")
 
 #End server
-message = "quit"
 socket.send_string(message)
 print(f"\nMessage sent: {message}\n")
